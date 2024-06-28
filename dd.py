b493aa62e8ce46c9673d7ce4b9573d5813165540
Apie <tim.fromentin.dev.pro@gmail.com>
2024-06-29 00:05:44 +0200

    clear

--- dd.py
+++
@@ -1,5 +0,0 @@
-from w3c_validator import validate
-
-messages = validate("http://www.google.com")["messages"]
-for m in messages:
-    print("Type: %(type)s, Line: %(lastLine)d, Description: %(message)s" % m)
--- main.py
+++ main.py
@@ -1,83 +1,134 @@
+# SECTION library
+from typing import List
 import discord
 from discord.ext import commands
-# import re
+from discord.ext.commands import has_permissions
 from csscompressor import compress as css_compress
+from discord.utils import MISSING
 from jsmin import jsmin
-
+import requests
+import ssl
+import socket
+import os
+from config import DISCORD_TOKEN
+# INTENTS
 intents = discord.Intents.all()
-intents.messages = True  # Activer les intents pour les messages
-
+intents.messages = True
 bot = commands.Bot(command_prefix="/", intents=intents)
 
 
-# @bot.command()
-# async def cssCorrector(ctx, *, arg):
-#     css = arg
-#     lexer = CssLexer()
-#     formatter = HtmlFormatter()
-#     result = highlight(css, lexer, formatter)
-#     print(result)
 
-@bot.command()
-async def minifieJS(ctx, *, arg):
-     js = arg
-     jsMinifie = jsmin(js)
-     await ctx.send(f" JS minifie ```js\n{jsMinifie}\n```")
 
-@bot.command()
-async def hey(ctx):
-    await ctx.send(f"Hey {ctx.author} !")
 
+# event laucnh bot
 @bot.event
 async def on_ready():
-    print("Ready !")
-
-@bot.command()
-async def serverInfo(ctx):
-    # on stock la props guild
-    server = ctx.guild
-    # on stock le nmbre de channel textuel
-    numberOfTextChannels= len(server.text_channels)
-    numberOfVoiceChannels = len(server.voice_channels)
-    numberOfPerson = server.member_count
-    serverName = server.name
-    message = f"Le serveur {serverName} compte {numberOfTextChannels} canaux textuels, {numberOfVoiceChannels} canaux vocaux et {numberOfPerson} membres."
-    await ctx.send(message)
-
-# @bot.command()
-# async def quijesuis(ctx):
-#     author = ctx.author
-#     authorName = author.name
-#     message = f"Je suis {authorName}"
-#     await ctx.send(message)
+    print(f'We have logged in as {bot.user}')
+    # Synchroniser les commandes de slash globales
+    try:
+        synced = await bot.tree.sync()
+        print(f"Synced {len(synced)} commands.")
+    except Exception as e:
+        print(f"Error syncing commands: {e}")
 
 
-# pas fini
-# @bot.command(description="Command for compress css")
-# async def compress(ctx, *, arg):
-#     message = arg
-#     # Remove multi-line comments (/* ... */)
-#     messageNoComment = re.sub(r'/\*.*?\*/', '', message, flags=re.DOTALL)
-#     # Compress spaces
-#     messageNoLine = ' '.join(messageNoComment.split())
-#     # messageNoSpace = messageNoLine.replace(" ", "")
-#     await ctx.send(messageNoLine)
+# SECTION message
 
-# compress 2
-@bot.command()
-async def minfieCSS(ctx, *, arg):
-    css = arg
-    compressed_css = css_compress(css)
-    await ctx.send(f"Minifie CSS :\n```css\n{compressed_css}\n```")
+
+
+message_bot_end = f"```markedown\n - ʀᴀᴄᴏᴏɴ 🦝 ```" 
+
+embed_template = discord.Embed(
+    title="- ʀᴀᴄᴏᴏɴ 🦝",
+    description="Bot create by",
+    color=discord.Color.dark_red()  
+)
+
+embed_template.set_footer(text="@Apie")
+embed_template.set_image(url="https://i.giphy.com/media/v1.Y2lkPTc5MGI3NjExeTRxN3UweXA3aXdzcXlwa3RwZjdoNGljOTJmdDdha3I2cWNtYm13eiZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/7CjEqtZUm2OBmJ9eha/giphy.gif")
 
 
 
 
+# SECTION minifie
+@bot.tree.command(name="minifie_js", description="Minifies a JS code")
+async def minifie_js(interaction: discord.Interaction, code: str):
+    js_minifie = jsmin(code)
+    await interaction.response.send_message(f"JS minifié: ```js\n{js_minifie}\n```")
 
 
-@bot.command(description="Command for creating doctype")
-async def doctype(ctx):
-    doctypeHtml = '''<!DOCTYPE html>
+
+    
+
+@bot.tree.command(name="minifie_css", description="Minifies a CSS code")
+async def minifie_css(interaction: discord.Interaction, code: str):
+    css_minifie = css_compress(code)
+    await interaction.response.send_message(f"CSS minifié: ```css\n{css_minifie}``` {message_bot_end}")
+
+# SECTION interaction
+@bot.tree.command(name="hey", description="Say Hey!")
+async def hey(interaction: discord.Interaction):
+    await interaction.response.send_message(f"```Hey {interaction.user} !```{message_bot_end} ")
+
+# ----------------
+@bot.tree.command(name="server_info", description="Server info")
+async def server_info(interaction: discord.Interaction):
+    server = interaction.guild
+    number_of_text_channels = len(server.text_channels)
+    number_of_voice_channels = len(server.voice_channels)
+    number_of_person = server.member_count
+    server_name = server.name
+    message = (f"The {server_name} server has {number_of_text_channels} text channels, "
+               f"{number_of_voice_channels} voice channels and {number_of_person} members.")
+    await interaction.response.send_message(f'{message} {message_bot_end}')
+
+# -----------------
+
+# Création de l'embed avec un lien dans la description
+embed_credit = discord.Embed(
+    title="Racoon developer",
+    description="Bot create by [Tim](https://discord.com/users/1242587565907775549)",
+    color=discord.Color.dark_red()  
+)
+
+embed_credit.set_author(name="Tim", url="https://github.com/apie-happy")
+embed_credit.set_image(url="https://i.giphy.com/media/v1.Y2lkPTc5MGI3NjExeTRxN3UweXA3aXdzcXlwa3RwZjdoNGljOTJmdDdha3I2cWNtYm13eiZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/7CjEqtZUm2OBmJ9eha/giphy.gif")
+embed_credit.set_footer(icon_url="https://i.pinimg.com/564x/4d/4e/68/4d4e68f0005e4bf67aead0778d690017.jpg", text="Tim")
+
+embed_credit.add_field(
+        name="Linkedin",
+        value="[Tim](https://www.linkedin.com/in/tim-fromentin-339b03208/)",
+        inline=False
+    )
+embed_credit.add_field(
+        name="Github",
+        value="[Apie](https://github.com/apie-happy)",
+        inline=False
+    )
+embed_credit.add_field(
+        name="Serveur",
+        value="[Développeur Français](https://discord.gg/qVW48MZVtj)",
+        inline=False
+    )
+embed_credit.add_field(
+        name="Profile photo",
+        value="[Raccoon Logo Vectors by Vecteezy](https://www.vecteezy.com/free-vector/raccoon-logo)",
+        inline=False
+    )
+
+
+# Commande pour envoyer un message avec l'embed de crédit
+@bot.tree.command(name="credit", description="Developer credit")
+async def credit(interaction: discord.Interaction):
+    await interaction.response.send_message(embed=embed_credit)
+
+
+
+
+# SECTION code
+@bot.tree.command(name="doctype", description="Create an HTML doctype")
+async def doctype(interaction: discord.Interaction):
+    doctype_html = '''<!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
@@ -88,58 +139,323 @@ async def doctype(ctx):
 
 </body>
 </html>'''
-    await ctx.send(f"```html\n{doctypeHtml}\n```")
+    await interaction.response.send_message(f"```html\n{doctype_html}``` {message_bot_end}")
+
+
+@bot.tree.command(name="lite_reset_css", description="Create a simple CSS reset")
+async def lite_reset_css(interaction: discord.Interaction):
+    reset = '''html{box-sizing:border-box}*,::after,::before{box-sizing:inherit}body{margin:0;padding:0}ol,ul{list-style:none}a{text-decoration:none;color:inherit'''
+    await interaction.response.send_message(f"```css\n{reset}``` {message_bot_end}")
+
+# FINISH
+
+@bot.tree.command(name="example_skeleton_css", description="Create a simple web folder skeleton in CSS")
+async def example_skeleton_css(interaction: discord.Interaction):
+    skeleton = '''
+├── public_html
+├── assets
+│   ├── img
+│   ├── font
+├── components
+│   ├── footer.html
+│   ├── navbar.html
+├── functions
+│   ├── db.php
+│   ├── function.php
+├── scripts
+│   ├── include.js
+│   ├── app.js
+├── styles
+│   └── global.css
+├── .gitignore
+├── pages
+└── index.html'''
+    await interaction.response.send_message(f"```markdown\n{skeleton}``` {message_bot_end}")
+
+# FINISH
+
+@bot.tree.command(name="example_skeleton_sass", description="Create a simple web folder skeleton in Sass")
+async def example_skeleton_sass(interaction: discord.Interaction):
+    skeleton = '''
+├── public_html
+├── assets
+│   ├── img
+│   ├── font
+├── components
+│   ├── footer.html
+│   ├── navbar.html
+├── functions
+│   ├── db.php
+│   ├── function.php
+├── scripts
+│   ├── include.js
+│   ├── app.js
+├── sass
+│   ├── base
+│   │   ├── _reset.scss
+│   │   ├── _typography.scss
+│   │   ├── index.scss
+│   ├── components
+│   │   ├── _button.scss
+│   │   ├── _card.scss
+│   │   ├── logo.scss
+│   ├── layouts
+│   │   ├── _footer.scss
+│   │   ├── _navbar.scss
+│   ├── themes
+│       ├── _themes.scss
+├── styles
+│   └── global.css
+├── .gitignore
+├── package.json
+├── pages
+└── index.html'''
+    await interaction.response.send_message(f"```markdown\n{skeleton}``` {message_bot_end}")
+
+
+
+
+
+
+
+# SECTION list
+# FINISH
+
+@bot.tree.command(name="best_utils", description="List of the best tools (in my opinion) for web development")
+async def utils_list(interaction: discord.Interaction):
+    utils_list_message = """ #List of utils :
+- Color palettes generator: <https://coolors.co>
+- Compress image: <https://compressimage.io>
+- Open source icons: <https://ionic.io/ionicons>
+- Front-end exercises: <https://www.frontendmentor.io/home>
+"""
+    await interaction.response.send_message(utils_list_message)
+
+# FINISH
+@bot.tree.command(name="best_vscode_extension", description="List of the best VSCode extensions (in my opinion) for web development")
+async def best_vscode_extension(interaction: discord.Interaction):
+    list_extension =   """
+# List :
+- Command Anchors: Place anchor tags within comments for easy file and workspace navigation
+- GitHub Pull Requests: Pull Request and Issue Provider for GitHub
+- Live Server
+- Project Templates: Create and apply custom project templates
+- px to rem & rpx & vw (cssrem): Converts between px and rem & rpx & vw units in VSCode
+"""
+    await interaction.response.send_message(f'{list_extension} {message_bot_end}')
+
+# SECTION website analyse
+@bot.tree.command(name="request_command", description="f")
+async def request_command(interaction: discord.Interaction, website: str):
+    request = requests.get(website)
+    request_status = request.status_code
+    request_url = request.url
+    request_request = request.request
+    request_encoding = request.encoding
+    await interaction.response.send_message(f'''Status: ```markedown\n{request_status}\n``` 
+    Url: ```markedown\n{request_url}\n```
+    Request: ```markedown\n{request_request}\n``` 
+    Encoding: ```markedown\n{request_encoding}``` 
+    {message_bot_end} ''')
+
+# SECTION tuto
+@bot.tree.command(name="tuto_code_send", description="d")
+async def tuto_code_send(interaction: discord.Interaction):
+    message = ("""to send code, put your code between backtick and add the language before the code.\n Example :\n \\```css 
+    .box {
+    background: #000;
+    color: white;
+}\\```
+  Result : ```css
+    .box {
+    background: #000;
+    color: white;
+}``` """)
+    await interaction.response.send_message(message + "\n" + message_bot_end)
+
+
+
+# SECTION ---------- admin
+
+# FINISH
+
+def is_admin(ctx):
+    return ctx.author.guild_permissions.administrator
+# FINISH
 
 @bot.command()
-async def liteResetCss(ctx):
-     reset = '''html{box-sizing:border-box}*,::after,::before{box-sizing:inherit}body{margin:0;padding:0}ol,ul{list-style:none}a{text-decoration:none;color:inherit'''
-     await ctx.send(f"```css\n{reset}\n```")
+@commands.check(is_admin)
+async def testAdmin(ctx):
+    await ctx.send(f'Salut {ctx.author.mention}, vous êtes administrateur! {message_bot_end}')
 
-@bot.command()
-async def exampleSqueleton(ctx):
-     squeleton = '''
-    ├── public_html
-    │ ├── components
-    │ └── footer.html
-    │ └── navbar.html
-    ├── functions
-    │ └── db.php
-    │ └── function.php
-    ├── scripts
-    │ └── include.js
-    │ └── app.js
-    ├── styles
-    │ └──  global.css
-    ├── .gitignore
-    ├── index.html'''
-     await ctx.send(f"```markedown\n{squeleton}\n```")
-
-@bot.command()
-async def utils_list(ctx):
-    utils_list_message = """List utils :
-    color palettes generator : <https://coolors.co>
-    compress image : <https://compressimage.io>
-    open source icons : <https://ionic.io/ionicons>
-    exercice front-end : <https://www.frontendmentor.io/home>
+# FINISH
+@bot.event
+async def on_command_error(ctx, error):
+    if isinstance(error, commands.CheckFailure):
+        message = """Your not admin
     """
-    await ctx.send(utils_list_message)
+        await ctx.send(f"```markedown\n{message}``` {message_bot_end}")
 
 
 
 
+# FINISH
 @bot.command()
-@commands.has_permissions(manage_messages=True)
+@commands.check(is_admin)
 async def clear(ctx, limit: int = None):
-        await ctx.channel.purge()
+    await ctx.channel.purge(limit=limit)
 
+
+
+
+# FIXME 
 @bot.command()
-async def credit(ctx):
-     await ctx.send('Bot create by [Tim](https://github.com/apie-happy), Logo [Raccoon Logo Vectors by Vecteezy](https://www.vecteezy.com/free-vector/raccoon-logo)')
+@commands.check(is_admin)
+async def new(ctx, *, arg):
+    title_news = arg
+    text_news = arg
+    link_news = arg
+    await ctx.send(f"{title_news}\n{text_news}\n{link_news}")
 
 
 
 
-bot.run("MTI1MzAzNDAwNTY3OTQ0MDAxNA.Gfkpme.ZSu9gXxCVUdp5gzzr6L0QugKGO1UTH8NSR_d8A")
+@bot.tree.command(name="commands", description="List of all slash commands")
+async def list_commands(interaction: discord.Interaction):
+    embed_command = discord.Embed(
+        title="Liste des commandes",
+        description="Voici une liste de toutes les commandes disponibles :",
+        color=discord.Color.blue()
+    )
+    
+    # Parcourir toutes les commandes enregistrées dans bot.tree
+    for cmd in bot.tree.get_commands():
+        embed_command.add_field(
+            name=f"/{cmd.name}",
+            value=cmd.description or "Pas de description disponible",
+            inline=False
+        )
+    embed_command.add_field(
+            name=f"/command_admin",
+            value=cmd.description,
+            inline=False
+        )
+    
+    embed_command.set_footer(text="- ʀᴀᴄᴏᴏɴ 🦝")
+    await interaction.response.send_message(embed=embed_command)
+
+
+@bot.command(name="command_admin", description="List of all commands")
+@commands.check(is_admin)
+async def command_admin(ctx):
+    embed = discord.Embed(
+        title="Liste des commandes",
+        description="Voici une liste de toutes les commandes disponibles:",
+        color=discord.Color.blue()
+    )
+    
+    # Parcourir toutes les commandes enregistrées
+    for cmd in bot.commands:
+        embed.add_field(
+            name=cmd.name,
+            value=cmd.description or "Pas de description disponible",
+            inline=False
+        )
+    
+    await ctx.send(embed=embed)
+   
+
+# SECTION ---------------------- test
+
+# @bot.command()
+# async def quijesuis(ctx):
+#     author = ctx.author
+#     authorName = author.name
+#     message = f"Je suis {authorName}"
+#     await ctx.send(message)
+
+
+# ------------------------
+
+# @bot.command(description="Command for compress css")
+# async def compress(ctx, *, arg):
+#     message = arg
+#     # Remove multi-line comments (/* ... */)
+#     messageNoComment = re.sub(r'/\*.*?\*/', '', message, flags=re.DOTALL)
+#     # Compress spaces
+#     messageNoLine = ' '.join(messageNoComment.split())
+#     # messageNoSpace = messageNoLine.replace(" ", "")
+#     await ctx.send(messageNoLine)
+# ------------------------------
+
+# @bot.command()
+# async def cssCorrector(ctx, *, arg):
+#     css = arg
+#     lexer = CssLexer()
+#     formatter = HtmlFormatter()
+#     result = highlight(css, lexer, formatter)
+#     print(result)
+
+
+# ---------------
+# @bot.tree.command(name="secure_website")
+# async def secure_website(interaction: discord.Interaction, website: str):
+#     def check_ssl(hostname):
+#         try:
+#             context = ssl.create_default_context()
+#             with socket.create_connection((hostname, 443)) as sock:
+#                 with context.wrap_socket(sock, server_hostname=hostname) as ssock:
+#                     ssock.do_handshake()
+#                 return True
+#         except ssl.SSLError:
+#             return False
+#         except Exception as e:
+#             print(f"An error occurred: {e}")
+#             return False
+    
+#     secure = check_ssl(website)
+    
+#     if secure:
+#         await interaction.response.send_message(f"{website} is secure (uses HTTPS). {message_bot_end}")
+#     else:
+#         await interaction.response.send_message(f"{website} is not secure (does not use HTTPS). {message_bot_end}")
+
+# -----------------------------
+
+
+
+
+
+# class languageDropDown(discord.ui.Select):
+#     def __init__(self, code: str):
+#         self.code = code
+#         options = [
+#             discord.SelectOption(label="HTML", description="html language"),
+#             discord.SelectOption(label="CSS", description="css language")
+#         ]
+#         super().__init__(placeholder="Sélectionnez le format du langage", options=options, min_values=1, max_values=1)
+
+#     async def callback(self, interaction: discord.Interaction):
+#         selectLanguage = self.values[0]
+#         if selectLanguage == "HTML":
+#             # Utiliser des backticks pour le bloc de code et ajouter 'html' pour la coloration syntaxique
+#             await interaction.response.send_message(f"```html\n{self.code}\n```")
+#         elif selectLanguage == "CSS":
+#             # Ajouter ici le traitement pour CSS si nécessaire
+#             await interaction.response.send_message(f"```css\n{self.code}\n```")
+
+# class languageView(discord.ui.View):
+#     def __init__(self, code: str):
+#         super().__init__()
+#         self.add_item(languageDropDown(code))
+
+# @bot.tree.command(name="language_formatage")
+# async def language_formatage(interaction: discord.Interaction, code: str):
+#     await interaction.response.send_message("Sélectionnez un langage :", view=languageView(code))
+
+
+
+bot.run(DISCORD_TOKEN)
 
 
 # photo
